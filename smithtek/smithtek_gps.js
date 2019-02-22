d4103558aa946050c5dd14912d50d3aec8b11820
Taras <php.laboratory@gmail.com>
2019-02-22 09:23:39 +0200

    Add GPS formatter module

--- smithtek/smithtek_gps.js
+++ smithtek/smithtek_gps.js
@@ -6,41 +6,69 @@ module.exports = function(RED){
         this.topic = config.topic;
 
         node.on('input', function(msg){
+            var m = {}
+            m.payload ={};
+            m.topic = node.topic;
+            m.payload["position"] = {"value":1, "context":{} };
 
-            const lat_arr = new Uint16Array(2);
-            lat_arr[0] = msg.payload[1];
-            lat_arr[1] = msg.payload[0];
 
-            var lat_nb = (new DataView(lat_arr.buffer)).getFloat32(0)
+            var lat_rawData = new ArrayBuffer(4);
+            var lat_intView = new Uint16Array(lat_rawData);
+            var lat_fltView = new Float32Array(lat_rawData);
+            lat_intView[0] = msg.payload[1]; //low
+            lat_intView[1] = msg.payload[0]; //high
+            var lat_nb= parseFloat(lat_fltView);
             var lat_raw = Math.abs(lat_nb)
             var lat_deg = Math.floor(lat_raw / 100)
             var lat_dec = (lat_raw - (100 * lat_deg)) / 60
+            m.payload["position"]["context"]["lat"]= (lat_deg + lat_dec)*Math.sign(lat_nb);
 
-
-            const lng_arr = new Uint16Array(2);
-            lng_arr[0] = msg.payload[3];
-            lng_arr[1] = msg.payload[2];
-            var lng_nb = (new DataView(lng_arr.buffer)).getFloat32(0)
+            var lng_rawData = new ArrayBuffer(4);
+            var lng_intView = new Uint16Array(lng_rawData);
+            var lng_fltView = new Float32Array(lng_rawData);
+            lng_intView[0] = msg.payload[3]; //low
+            lng_intView[1] = msg.payload[2]; //high
+            var lng_nb= parseFloat(lng_fltView);
             var lng_raw = Math.abs(lng_nb)
             var lng_deg = Math.floor(lng_raw / 100)
             var lng_dec = (lng_raw - (100 * lng_deg)) / 60
-
-            var m = {}
-            m.payload ={};
-            m.payload["position"] = {"value":1, "context":
-                {    'lat': (lat_deg + lat_dec)*Math.sign(lat_nb),
-                    'lng': (lng_deg + lng_dec)*Math.sign(lng_nb)
-                }
-            };
-            m.topic = node.topic;
+            m.payload["position"]["context"]["lng"]= (lng_deg + lng_dec)*Math.sign(lng_nb);
 
 
-            node.status({fill:"blue",shape:"ring",
-                text:
-                    m.topic  + ":" +
-                    "lat:" + m.payload["position"]["context"]["lat"] +
-                    ",lng:"+ m.payload["position"]["context"]["lng"]
-            });
+
+
+            // const lat_arr = new Uint16Array(2);
+            // lat_arr[0] = msg.payload[1];
+            // lat_arr[1] = msg.payload[0];
+            //
+            // var lat_nb = (new DataView(lat_arr.buffer)).getFloat32(0)
+            // var lat_raw = Math.abs(lat_nb)
+            // var lat_deg = Math.floor(lat_raw / 100)
+            // var lat_dec = (lat_raw - (100 * lat_deg)) / 60
+            //
+            //
+            // const lng_arr = new Uint16Array(2);
+            // lng_arr[0] = msg.payload[3];
+            // lng_arr[1] = msg.payload[2];
+            // var lng_nb = (new DataView(lng_arr.buffer)).getFloat32(0)
+            // var lng_raw = Math.abs(lng_nb)
+            // var lng_deg = Math.floor(lng_raw / 100)
+            // var lng_dec = (lng_raw - (100 * lng_deg)) / 60
+            //
+            // m.payload["position"] = {"value":1, "context":
+            //     {    'lat': (lat_deg + lat_dec)*Math.sign(lat_nb),
+            //         'lng': (lng_deg + lng_dec)*Math.sign(lng_nb)
+            //     }
+            // };
+
+
+
+            // node.status({fill:"blue",shape:"ring",
+            //     text:
+            //         m.topic  + ":" +
+            //         "lat:" + m.payload["position"]["context"]["lat"] +
+            //         ",lng:"+ m.payload["position"]["context"]["lng"]
+            // });
 
            node.send(m);
         });
